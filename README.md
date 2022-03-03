8e68364053028f072bba5ed84c462a1ac7e99b54
shaijing <yulingsc@outlook.com>
2022-03-03 19:15:51 +0800

    Add p1093

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# pat
+# online judge
 ## Note:use gcc above 8, pat1002 will not work 2022_02-21.
 ## It's very strange that pat1002 runs will in 2022_0222.
---
+++ p1093.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct stud {
+    int id;
+    int ch;
+    int ma;
+    int en;
+    int sum;
+} stu;
+
+int mycmp(const void*, const void*);
+int main() {
+    int n, i;
+    stu st[301];
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+        scanf("%d %d %d", &st[i].ch, &st[i].ma, &st[i].en);
+        st[i].sum = st[i].ch + st[i].ma + st[i].en;
+        st[i].id = i + 1;
+    }
+    qsort(&st[0], n, sizeof(stu), mycmp);
+    for (i = 0; i < 5; i++) {
+        printf("%d %d\n", st[i].id, st[i].sum);
+    }
+
+    return 0;
+}
+int mycmp(const void* p1, const void* p2) {
+    const stu* ptr1 = (stu*)p1;
+    const stu* ptr2 = (stu*)p2;
+    if (ptr1->sum > ptr2->sum) {
+        return -1;
+    } else if (ptr1->sum == ptr2->sum) {
+        if (ptr1->ch >= ptr2->ch) {
+            return -1;
+        } else
+            return 1;
+    } else
+        return 1;
+}
